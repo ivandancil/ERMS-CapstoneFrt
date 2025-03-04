74f7ff0beea27553c606f93096f4f8d5a97328a5
Ivan Dancil <ivanbdancil@gmail.com>
2025-03-04 20:50:59 +0700

    Added USER Components

---
+++ src/pages/user/ProfileSetting.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material"
+import Header from "../../components/Header"
+
+
+const ProfileSetting = () => {
+  return (
+    <Box m='20px'>
+    <Box display='flex' justifyContent='space-between' alignItems='center'>
+      <Header title="PROFILE" subtitle="" />
+    </Box>
+  </Box>
+  )
+}
+
+export default ProfileSetting

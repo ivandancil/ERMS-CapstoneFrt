0356ab6b7c9dae7be2e2e099c3a192761c642986
Ivan Dancil <ivanbdancil@gmail.com>
2025-03-30 18:31:14 +0700

    Updated

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -7,11 +7,7 @@ import { tokens } from "../../theme";
 function Login() {
   const theme = useTheme();
   const navigate = useNavigate();
-<<<<<<< HEAD
-   const colors = tokens(theme.palette.mode);
-=======
   const colors = tokens(theme.palette.mode);
->>>>>>> frontend
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
